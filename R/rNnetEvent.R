1a6bbe70609864f514deefe062e884bd058b11f0
Bogdan Oancea <bogdan.oancea@gmail.com>
2020-07-19 16:02:33 +0300

    first commit

---
+++ R/rNnetEvent.R
@@ -0,0 +1,73 @@
+
+rNnetEvent <- function(dupFileName, regsFileName, posLocPath) {
+# 1. read duplicity probs.
+
+if (!file.exists(dupFileName))
+  stop(paste0(dupFileName, " does not exists!"))
+
+dupProbs <- fread(
+  dupFileName,
+  sep = ',',
+  header = TRUE,
+  stringsAsFactors = FALSE
+)
+devices<-as.numeric(dupProbs[,1][[1]])
+
+# 2. read regions
+if (!file.exists(regsFileName))
+  stop(paste0(regsFileName, " does not exists!"))
+
+regions <- fread(
+  regFileName,
+  sep = ',',
+  header = TRUE,
+  stringsAsFactors = FALSE
+)
+
+# 3. read posterior location probabilities
+ndevices <- nrow(dupProbs)
+postLoc<-list(length = ndevices)
+for( i in 1:ndevices) {
+  postLoc[[i]] <- readPostLocProb(postLocPath, dupProbs[i,1])
+}
+
+T <- ncol(postLoc[[1]])
+nTiles<-nrow(postLoc[[1]])
+
+
+# 4. computation begins ...
+#### parallel  
+# build cluster
+library(parallel)
+  
+if (Sys.info()[['sysname']] == 'Linux' |
+  Sys.info()[['sysname']] == 'Darwin') {
+  cl <- makeCluster(detectCores(), type = "FORK")
+} else {
+  cl <- makeCluster(detectCores())
+  clusterEvalQ(cl, library("data.table"))
+  clusterEvalQ(cl, library("deduplication"))
+  clusterEvalQ(cl, library("extraDistr"))
+  clusterExport(cl, c('postLoc', 'devices', 'ndevices', 'nTiles', 'dupProbs', 'regions', 'rNnet_Event', 'nIndividuals'), envir = environment())
+}
+ichunks <- clusterSplit(cl, 1:T)
+
+res <-
+  clusterApplyLB(
+    cl,
+    ichunks,
+    doAggr,
+    ndevices,
+    nTiles,
+    postLoc,
+    devices,
+    dupProbs,
+    regions
+  )
+stopCluster(cl)
+result <- rbindlist(res)
+return (result)
+}
+
+
+
